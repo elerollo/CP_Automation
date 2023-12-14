bc673e3ca8be67f2e624ed8cc691b8490af0b302
elerollo <iamerykvest@gmail.com>
2023-12-14 19:27:53 +0100

    New check extension and workweek function, optimized gpu resolution function New figma project

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import random
-from FPS_tool import *
-from STREAM_tool import *
+import datetime
 
 capture_app = "MissingCaptureAppInfo"
 application_name = "MissingApplicationNameInfo"
@@ -16,15 +15,27 @@ run_number = "MissingRunNumber"
 old_file_directory = "NoData"
 graphic_preset = "MissingPreset"
 graphic_resolution = "MissingResolution"
-raytracing = ""
-driver = ""
-upscaller = ""
+raytracing = "MissingRaytracingInfo"
+driver = "MissingDriverInfo"
+upscaller = "MissingUpscalerInfo"
+file_extension = "MissingFileExtensionInfo"
+todays_date = datetime.date.today()
+
+gpu_resolution_map = {
+    'A380': '1080p',
+    'A750': '1440p',
+    'A770': '1440p',
+    '4060': '1440p',
+    '3060': '1440p'
+}
+
+
+def check_date(date):
+    return datetime.date(date.year, date.month, date.day).isocalendar().week
+
 
 def update_data():
     global capture_app, application_name, api, work_week
-
-    print('Capture app:')
-    capture_app = input()
 
     print('Application name:')
     application_name = input()
@@ -32,8 +43,6 @@ def update_data():
     print('API:')
     api = input()
 
-    print('Workweek:')
-    work_week = input()
 
 def create_random_file():
     template_name = "CX_2023-12-11_13-53-54_Starfield_"
@@ -43,11 +52,11 @@ def create_random_file():
     all_extra_settings = ["DLSS", "XeSS", "RT", ""]
     all_drivers = ["Base", "AIL", ""]
 
-    random_extra_setting = all_extra_settings[random.randint(0, 3)]
-    random_gpu = all_gpus[random.randint(0, 4)]
-    random_scene_name = all_scenes[random.randint(0, 3)]
+    random_extra_setting = random.choice(all_extra_settings)
+    random_gpu = random.choice(all_gpus)
+    random_scene_name = random.choice(all_scenes)
     random_run_number = random.randint(1, 5)
-    random_driver = all_drivers[random.randint(0, 2)]
+    random_driver = random.choice(all_drivers)
 
     template_name = template_name + random_gpu + "_" + random_scene_name + "_" + str(random_run_number) + not_empty(random_extra_setting) + not_empty(random_driver) + ".png"
     return template_name
@@ -60,6 +69,7 @@ def test(number_of_files):
         random_file = open(path + "/" + create_random_file(), 'w')
         random_file.close()
 
+
 def is_raytracing_on():
     global graphic_resolution, raytracing
 
@@ -69,8 +79,25 @@ def is_raytracing_on():
     else:
         raytracing = ""
 
+
+def check_extension(file_name):
+    global file_extension
+    file_extension = file_name.split('.')[1]
+
+    if file_extension == "png":
+        return "FPS"
+    elif file_extension == "etl":
+        return "ETL"
+    elif file_extension == "lcs2":
+        return "GFXBench"
+    elif file_extension == "pix":
+        return "PIX"
+    else:
+        return "Unknown"
+
+
 def rename():
-    global directoryContent, upscaller, working_name, scene_name, run_number, gpu, old_file_directory, graphic_resolution, extra_comment, driver
+    global directoryContent, todays_date, upscaller, working_name, scene_name, run_number, gpu, old_file_directory, graphic_resolution, extra_comment, driver, capture_app, graphic_preset, work_week
 
     directoryContent = os.listdir(path)
 
@@ -95,41 +122,29 @@ def rename():
             else:
                 upscaller = ""
 
-            if working_name.find('A380') != -1:
-                gpu = "A380"
-                graphic_resolution = "1080p"
-            elif working_name.find('A750') != -1:
-                gpu = "A750"
-                graphic_resolution = "1440p"
-            elif working_name.find('A770') != -1:
-                gpu = "A770"
-                graphic_resolution = "1440p"
-            elif working_name.find('4060') != -1:
-                gpu = "4060"
-                graphic_resolution = "1440p"
-            elif working_name.find('3060') != -1:
-                gpu = "3060"
-                graphic_resolution = "1440p"
-            else:
-                gpu = "MissingGPUInfo"
-
-            is_raytracing_on()
+            for gpu_name, resolution in gpu_resolution_map.items():
+                if working_name.find(gpu_name) != -1:
+                    gpu = gpu_name
+                    graphic_resolution = resolution
+                    is_raytracing_on()
 
             if gpu == "A380":
                 graphic_preset = "High"
+            elif working_name.find('High') != -1:
+                graphic_preset = "High"
+            elif working_name.find('Epic') != -1:
+                graphic_preset = "Epic"
+            elif working_name.find('VeryHigh') != -1:
+                graphic_preset = "VeryHigh"
             else:
                 graphic_preset = "Ultra"
 
+            capture_app = check_extension(working_name)
+            work_week = 'WW' + str(check_date(todays_date))
 
             old_file_directory = path + '/' + oldFile
             new_name = (path + '/' + capture_app + '_' + application_name + '_' + api + '_' + gpu + '_' + graphic_resolution + "_" + graphic_preset + not_empty(upscaller) + not_empty(raytracing) + '_' + scene_name + '_' + work_week + '_Run' + run_number + not_empty(driver) + '.png')
             os.rename(old_file_directory, new_name)
-            print(capture_app, application_name, api, work_week, scene_name, run_number, graphic_preset, graphic_resolution, raytracing, driver, upscaller, gpu)
-# gpu
-# # path
-# # directoryContent = []
-# # old_file_directory = "NoData"
-# graphic_preset = "MissingPreset"
 
 
 def not_empty(file):
@@ -138,7 +153,8 @@ def not_empty(file):
     else:
         file = "_" + file
         return file
-test(12)
+
+
+test(1)
 update_data()
 rename()
-
